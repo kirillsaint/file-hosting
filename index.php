6f01019fc370d01af8627d59f1c65cf8e51d0ffc
kirillsaint <kirillsaint@wtfuck.tk>
2021-10-04 19:08:48 +0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,5 @@
+<h2>ShareFile by kirillsaint</h2>
+<form method="post" action="core/upload.php" enctype="multipart/form-data">
+    <input type="file" name="image">
+    <button type="submit">Upload</button>
+</form>
